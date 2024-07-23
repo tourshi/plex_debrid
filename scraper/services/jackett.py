d2494ebb5342a3263fc1c934bb826df94b34da59
David Young <davidy@funkypenguin.co.nz>
2024-07-24 11:29:01 +1200

    fix: unescaped quote breaking jackett integration
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- scraper/services/jackett.py
+++ scraper/services/jackett.py
@@ -66,7 +66,7 @@ def scrape(query, altquery):
             ui_print('[jackett] error: jackett request timed out. Reduce the number of jackett indexers, make sure your indexers are healthy and enable the jackett setting "CORS".')
             return []
         except :
-            ui_print('[jackett] error: jackett couldn't be reached. Make sure your jackett base url is correctly formatted (default: http://jackett:9117).')
+            ui_print('[jackett] error: jackett couldn\'t be reached. Make sure your jackett base url is correctly formatted (default: http://jackett:9117).')
             return []
         if not response.status_code == 200:
             if response.status_code in [401,403]:
