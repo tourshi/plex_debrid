12434538a1cbb248417e780fe0bb088941dbec95
sirstudly <ron1404@techie.com>
2024-12-18 19:51:55 +0000

    resolves #22, [alldebrid] removes endpoint /magnet/instant; updates to use API v4.1

--- debrid/services/alldebrid.py
+++ debrid/services/alldebrid.py
@@ -9,7 +9,8 @@ short = "AD"
 # (required) Authentification of the Debrid service, can be oauth aswell. Create a setting for the required variables in the ui.settings_list. For an oauth example check the trakt authentification.
 api_key = ""
 # Define Variables
-session = requests.Session()
+rate_limit_sec = 1 / 12  # minimum number of seconds between requests
+session = custom_session(get_rate_limit=rate_limit_sec, post_rate_limit=rate_limit_sec)
 
 def setup(cls, new=False):
     from debrid.services import setup
@@ -38,6 +39,7 @@ def get(url):
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
         'authorization': 'Bearer ' + api_key}
     try:
+        ui_print("[alldebrid] (get): " + url, debug=ui_settings.debug)
         response = session.get(url + '&agent=plex_debrid', headers=headers)
         logerror(response)
         response = json.loads(response.content, object_hook=lambda d: SimpleNamespace(**d))
@@ -52,6 +54,7 @@ def post(url, data):
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
         'authorization': 'Bearer ' + api_key}
     try:
+        ui_print("[alldebrid] (post): " + url + " with data " + repr(data), debug=ui_settings.debug)
         response = session.post(url, headers=headers, data=data)
         logerror(response)
         response = json.loads(response.content, object_hook=lambda d: SimpleNamespace(**d))
@@ -68,54 +71,61 @@ def download(element, stream=True, query='', force=False):
     for release in cached[:]:
         # if release matches query
         if regex.match(r'(' + query + ')', release.title, regex.I) or force:
-            if stream:
-                # Cached Download Method for AllDebrid
-                url = 'https://api.alldebrid.com/v4/magnet/instant?magnets[]=' + release.download[0]
-                response = get(url)
-                instant = False
+            debrid_uncached = True
+            for i, rule in enumerate(element.version.rules):
+                if rule[0] == "cache status" and rule[1] in ['requirement', 'preference'] and rule[2] == "cached":
+                    debrid_uncached = False
+                    break
+            if not debrid_uncached:  # Cached Download Method for AllDebrid
+                torrent_id = None
                 try:
-                    instant = response.data.magnets[0].instant
-                except:
-                    continue
-                if instant:
                     url = 'https://api.alldebrid.com/v4/magnet/upload?magnets[]=' + release.download[0]
                     response = get(url)
+                    if not hasattr(response, "status") or response.status != "success":
+                        ui_print(f"[alldebrid] failed to add release {release.title}.")
+                        continue
+                    if not hasattr(response, "data") or not hasattr(response.data.magnets[0], "id"):
+                        ui_print(f"[alldebrid] failed to add release {release.title}.")
+                        if hasattr(response.data.magnets[0], "error"):
+                            ui_print(f"[alldebrid] error: {response.data.magnets[0].error.message}.")
+                        continue
+
+                    # check if release is instantly available
                     torrent_id = response.data.magnets[0].id
-                    url = 'https://api.alldebrid.com/v4/magnet/status?id=' + str(torrent_id)
+                    url = 'https://api.alldebrid.com/v4.1/magnet/status?id=' + str(torrent_id)
                     response = get(url)
-                    torrent_files = response.data.magnets.links
-                    torrent_links = []
-                    for file in torrent_files:
-                        torrent_links += [file.link]
-                    if len(torrent_links) > 0:
-                        rate_limit = 1 / 12
-                        success = False
-                        saved_links = []
-                        for link in torrent_links:
-                            url = 'https://api.alldebrid.com/v4/link/unlock?link=' + requests.utils.quote(link)
-                            response = get(url)
-                            if not response.status == 'success':
-                                success = False
-                                break
-                            saved_links += [requests.utils.quote(link)]
-                            success = True
-                            time.sleep(rate_limit)
-                        if success:
-                            saved_links = '&links[]='.join(saved_links)
-                            url = 'https://api.alldebrid.com/v4/user/links/save?links[]=' + saved_links
-                            response = get(url)
-                            ui_print('[alldebrid] adding cached release: ' + release.title)
-                            return True
-                        else:
-                            # delete failed torrent
-                            return False
+                    if response.status != "success" or response.data.magnets.status != "Ready":
+                        raise Exception(f"{release.title} is in status '{response.data.magnets.status}' (not cached).")
+
+                    saved_links = []
+                    for link in get_all_links_from_files(response.data.magnets.files):
+                        url = 'https://api.alldebrid.com/v4/link/unlock?link=' + requests.utils.quote(link)
+                        response = get(url)
+                        if response.status != 'success':
+                            raise Exception(f"{response.status} response on unlock link for {release.title}.")
+                        saved_links += [requests.utils.quote(link)]
+
+                    if len(saved_links) > 0:
+                        saved_links = '&links[]='.join(saved_links)
+                        url = 'https://api.alldebrid.com/v4/user/links/save?links[]=' + saved_links
+                        response = get(url)
+                        if response.status != "success":
+                            raise Exception(f"failed to save links for {release.title}.")
+                        ui_print(f"[alldebrid] adding cached release: {release.title}")
+                        return True
                     else:
-                        # delete failed torrent
-                        return False
+                        raise Exception(f"no files found for {release.title}")
+                except Exception as e:
+                    ui_print("[alldebrid] " + str(e) + " Looking for another release.")
+                    if torrent_id is not None:
+                        get('https://api.alldebrid.com/v4/magnet/delete?id=' + str(torrent_id))
             else:
                 # Uncached Download Method for AllDebrid
                 url = 'https://api.alldebrid.com/v4/magnet/upload?magnets[]=' + release.download[0]
                 response = get(url)
+                if not hasattr(response, "status") or response.status != "success":
+                    ui_print(f"[alldebrid] failed to add release {release.title}.")
+                    continue
                 ui_print('[alldebrid] adding uncached release: ' + release.title)
                 return True
     return False
@@ -127,21 +137,19 @@ def check(element, force=False):
     else:
         wanted = element.files()
     unwanted = releases.sort.unwanted
-    hashes = []
+    wanted_patterns = list(zip(wanted, [regex.compile(r'(' + key + ')', regex.IGNORECASE) for key in wanted]))
+    unwanted_patterns = list(zip(unwanted, [regex.compile(r'(' + key + ')', regex.IGNORECASE) for key in unwanted]))
     for release in element.Releases[:]:
-        if len(release.hash) == 40:
-            hashes += [release.hash]
-        else:
-            element.Releases.remove(release)
-    if len(hashes) > 0:
-        response = get(
-            'https://api.alldebrid.com/v4/magnet/instant?magnets[]=' + '&magnets[]='.join(hashes[:200]))
-        for i, release in enumerate(element.Releases):
-            try:
-                instant = response.data.magnets[i].instant
-                if instant:
-                    release.cached += ['AD']
-                    # release.wanted = 0
-                    # release.unwanted = 0
-            except:
-                continue
+        release.wanted_patterns = wanted_patterns
+        release.unwanted_patterns = unwanted_patterns
+        release.maybe_cached += ['AD']  # we won't know if it's cached until we attempt to download it
+
+
+def get_all_links_from_files(files):
+    links = []
+    for file in files:
+        if hasattr(file, "e"):  # is folder
+            links += get_all_links_from_files(file.e)
+        elif hasattr(file, "l"):  # is a file
+            links += [file.l]
+    return links
